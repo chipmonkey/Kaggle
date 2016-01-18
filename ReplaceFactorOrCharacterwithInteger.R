bb3cdfe6303a4c10c6241a75e42cba575f3337a1
Chip Lynch <chip@chiplynch.com>
2016-01-18 00:32:04 -0500

    Add julian date function to type conversion
    Added julian dates, fixed as.character chaining issue from factor -> character -> integer.

--- ReplaceFactorOrCharacterwithInteger.R
+++ ReplaceFactorOrCharacterwithInteger.R
@@ -4,21 +4,27 @@
 # between test and train, when the two may have different sets of values
 #
 
-featurenames <- intersect(names(train), names(test))
-featurenames <- setdiff(featurenames, c('id', 'date_first_booking', 'country_destination'))  # Adjust as needed
-
+cat('Converting character and factor types to integers')
 for (i in featurenames) {
-  if (class(train[,i]) != class(test[,i])) { print("Warning: type mismatch between test and dev") } # We could exit, but meh
-  if (class(train[,i]) == 'factor') {             # Convert everything to character first
-    train[,i] <- as.character(train[,i])
-    test[,i] <- as.character(test[,i])
+  print(paste(i, class(train[[i]])))
+  colname = paste0(i, '_num', sep='')
+  if (class(train[,i]) != class(test[,i])) { print(paste("Warning: type mismatch between test and dev:", i)) }
+  if (class(train[,i]) == 'factor') {
+    train[[i]] <- as.character(train[,i])  # i not colname in the assignment so it will be handled
+    test[[i]] <- as.character(test[,i])    # in the next step
   }
   
   if (class(train[[i]])=="character") {
-    mylevels <- unique(c(train[[i]], test[[i]]))  # build a list of alllll the levels
-    colname = paste0(i, '_num', sep='')
-    train[[colname]] <- as.integer(factor(train[[i]], levels=mylevels))  # use the same levels list
-    test[[colname]]  <- as.integer(factor(test[[i]],  levels=mylevels))  # in test and train
+    mylevels <- unique(c(train[[i]], test[[i]]))
+    train[[colname]] <- as.integer(factor(train[[i]], levels=mylevels))
+    test[[colname]]  <- as.integer(factor(test[[i]],  levels=mylevels))
+  }
+  
+  if (class(train[[i]])=="Date") {
+    train[[colname]] <- julian(train[[i]]) 
+    test[[colname]] <- julian(test[[i]])
+  }
+  if(class(train[[i]]) %in% c('factor', 'character', 'Date')) {
     featurenames <- setdiff(featurenames, i)
     featurenames <- union(featurenames, colname)
   }
