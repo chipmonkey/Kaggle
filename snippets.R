c53b252b8a80de4c49cf31295698ee039a08cbac
Chip Lynch <chip@chiplynch.com>
2016-08-28 17:51:24 -0400

    Added initialize and tracking bits
    Garbage collection, a simple timer, and an rm -r * function (to be used with care at the beginning of life... with care, and carefully).

--- snippets.R
+++ snippets.R
@@ -3,3 +3,10 @@
 # Which don't belong in any other real category
 #
 options(scipen = 999) # Mostly disable scientific notation in display and file writes
+options(stringsAsFactors=F)   # Disable conversion of strings to factors on read.  Sometimes useful.
+
+rm(list=ls());  # WARNING!  this is effectively rm -r /*  !  Destroy everything!  Use with care!
+
+gc() ; Sys.time() ; start_time <- Sys.time()  # Run this at the beginning
+gc() ; Sys.time() - start_time  # And run this at every useful checkpoint afterwards to do garbage collection and track time
+
