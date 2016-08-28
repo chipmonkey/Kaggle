af7fa532efffff1d9da6a06340354b3b3a430b12
Chip Lynch <chip@chiplynch.com>
2016-08-28 16:50:45 -0400

    Created snippets
    Some generally useful but really simple things I forget about in R which don't belong in any other real category.

---
+++ snippets.R
@@ -0,0 +1,5 @@
+#
+# Some generally useful but really simple things I forget about in R
+# Which don't belong in any other real category
+#
+options(scipen = 999) # Mostly disable scientific notation in display and file writes
