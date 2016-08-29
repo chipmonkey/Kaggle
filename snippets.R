bfb74d3e14db73c4b2a6df4069c01fd4421b10bd
Chip Lynch <chip@chiplynch.com>
2016-08-29 16:56:59 -0400

    Added filename timestamp
    Added a quick snippet to generate unique filenames so we don't have to worry about that any more.

--- snippets.R
+++ snippets.R
@@ -10,3 +10,6 @@ rm(list=ls());  # WARNING!  this is effectively rm -r /*  !  Destroy everything!
 gc() ; Sys.time() ; start_time <- Sys.time()  # Run this at the beginning
 gc() ; Sys.time() - start_time  # And run this at every useful checkpoint afterwards to do garbage collection and track time
 
+file_timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")  # Generate a timestamp with 1-second accuracy
+train_filename <- paste('../Interim/one_hot_encode_xgboost_borrowed_train_', file_timestamp, '.csv', sep="")  # Create a timestamp filename
+
